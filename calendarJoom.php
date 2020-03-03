dc877532a74d9424719ee1b07211dbc3baadff59
Israel Kernel <israelaguilarcruz@gmail.com>
2020-03-03 08:16:18 -0600

    Calendario estilo antiguo desarrollado con JS & CSS

---
+++ calendarJoom.php
@@ -0,0 +1,32 @@
+{source}
+<?php
+//LINEA DE SEGURIDAD
+defined('_JEXEC') or die;
+
+//DEFINR LAS RUTAS
+$ruta_c=JURI::base()."templates/";
+$ruta_cz=JURI::base();
+
+?>
+<!DOCTYPE html>
+
+    			<!-- ETIQUETA HTML REPLAZA LAS TERMINACIONES no-js POR js -->
+
+    			<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+    			<!--[if IE 7 ]> <html lang="en" class="no-js ie7"> <![endif]-->
+    			<!--[if IE 8 ]> <html lang="en" class="no-js ie8"> <![endif]-->
+    			<!--[if IE 9 ]> <html lang="en" class="no-js ie9"> <![endif]-->
+    			<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+    			
+    
+    <head>
+     <meta name="viewport" content="initial-scale=1, width=device-width">
+     
+    	<script src="<?php echo $ruta_c;?>costanzo001/js/calendarJS.js"></script>
+    	<link rel="stylesheet" href="<?php echo $ruta_c;?>costanzo001/css/calendarCSS.css" />
+    	
+    </head>
+    <body class="homepage">
+    </body>
+    
+</html>
